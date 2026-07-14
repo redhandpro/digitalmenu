53c0b9dd742cc3a3f453a817db8daf6bd94da07c
redhandpro <ariaa1968@gmail.com>
2026-07-14 17:20:33 +0330

    Add business category and product models

---
+++ business/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Business(models.Model):
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=100)
+
+    logo = models.ImageField(
+        upload_to='logos/',
+        blank=True,
+        null=True
+    )
+
+    address = models.TextField(
+        blank=True
+    )
+
+    phone = models.CharField(
+        max_length=20,
+        blank=True
+    )
+
+    slug = models.SlugField(
+        unique=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def str(self):
+        return self.name
+
+
+
+class Category(models.Model):
+    business = models.ForeignKey(
+        Business,
+        on_delete=models.CASCADE,
+        related_name="categories"
+    )
+
+    name = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+
+    name = models.CharField(max_length=100)
+
+    description = models.TextField(
+        blank=True
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=0
+    )
+
+    image = models.ImageField(
+        upload_to="products/",
+        blank=True,
+        null=True
+    )
+
+    available = models.BooleanField(
+        default=True
+    )
+
+    def str(self):
+        return self.name
