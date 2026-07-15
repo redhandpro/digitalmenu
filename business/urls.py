820748550794391cfcfccc6c1f1a21eca4780775
redhandpro <ariaa1968@gmail.com>
2026-07-15 15:06:36 +0330

    Add QR code generation for business menu

--- business/urls.py
+++ business/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
+
     path(
         "menu/<slug:slug>/",
         views.menu_view,
         name="menu"
     ),
+
+    path(
+        "qr/<slug:slug>/",
+        views.qr_code_view,
+        name="qr"
+    ),
+
 ]
--- business/views.py
+++ business/views.py
@@ -18,3 +18,29 @@ def menu_view(request, slug):
             "categories": categories
         }
     )
+import qrcode
+from io import BytesIO
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+
+def qr_code_view(request, slug):
+
+    business = get_object_or_404(
+        Business,
+        slug=slug
+    )
+
+    url = request.build_absolute_uri(
+        f"/menu/{business.slug}/"
+    )
+
+    qr = qrcode.make(url)
+
+    buffer = BytesIO()
+    qr.save(buffer, format="PNG")
+
+    return HttpResponse(
+        buffer.getvalue(),
+        content_type="image/png"
+    )
